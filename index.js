99ad0352a30db7eec11af40f7d570884c6f480a0
TKEugene <eugenewem@gmail.com>
2019-06-11 17:16:49 +0300

    I added nodemon

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require ("express");
 const mysql = require("mysql");
 const app = express();
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "foo",
-    password: "password",
-    database: "drug_listing"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 app.get("/api/chemist", (req, res) => {
