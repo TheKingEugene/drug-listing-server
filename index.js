903b953f391f6486d2000673a8de1960cd02d86d
TKEugene <eugenewem@gmail.com>
2019-06-11 13:57:04 +0300

    Created pool and database connection

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require ("express");
+const mysql = require("mysql");
 const app = express();
-
-app.get("/", function (req, res){
-    //Logic
-    res.send("Long Live The King Eugene")
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "foo",
+    password: "password",
+    database: "drug_listing"
 });
 
-app.listen(9000, function(){
-    console.log("App listening to port 9000");
+app.get("/api/chemist", (req, res) => {
+    //fetching from chemist
+    pool.query("SELECT Business_Number, Name FROM chemist", (error, rows) => {
+        if (error){
+            return res.status(500).json({error});
+        }
+
+        res.json(rows);
+
+    });
+    
 });
+
+app.listen(9000, () => console.log("App listening to port 9000"));
