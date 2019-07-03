f64e418b322b6a1063df2ec9893973b9c2b0cfcc
TKEugene <eugenewem@gmail.com>
2019-07-04 00:32:16 +0300

    Redone the POST endpoints

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get("/api/drug/", (req, res) => {
     );
 });
 
-/*
+
 app.post("/api/Chemist", (req, res) => {
     const { Chemist, Location, Business_Number } = req.body;
 
@@ -114,110 +114,7 @@ app.post("/api/Chemist_Contacts", (req, res) => {
             res.json(results.affectedRows);
         }
     );
-}); */
-
-app.post("/api/drug_listing", (req, res) => {
-    const { Chemist, Location, Business_Number } = req.body;
-
-    if (!Chemist && !Location && !Business_Number) {
-        return res.status(400).json({ error: "Invalid payload" })
-    }
-
-    pool.getConnection((error, connection) => {
-        if (error) {
-            return res.status(500).json({ error });
-        }
-
-        connection.beginTransaction(error => {
-            if (error) {
-                return res.status(500).json({ error });
-            }
-            connection.query(
-                "INSERT INTO Chemist (Chemist, Location, Business_Number) VALUES(?, ?, ?)",
-                [Chemist, Location, Business_Number],
-                (error, results) => {
-                    if (error) {
-                        return connection.rollback(() => {
-                            res.status(500).json({ error });
-                        });
-                    }
-
-                    const insertId = results.insertId;
-                    const Drug = Drug.map(Drug => [insertId, Drug]);
-                    const Price = Price.map(Price => [insertId, Price]);
-                    const Manufacturer = Manufacturer.map(Manufacturer => [insertId, Manufacturer]);
-                    const Discription = Discription.map(Discription => [insertId, Discription]);
-                    const Drug_Number = Drug_Number.map(Drug_Number => [insertId, Drug_Number]);
-                    const Business_Number = Business_Number.map(Business_Number => [insertId, Business_Number]);
-
-                    connection.query(
-                        "INSERT INTO Drug (Drug, Price, Manufacturer, Discription, Drug_Number, Business_Number)",
-                        [Drug, Price, Manufacturer, Discription, Drug_Number, Business_Number],
-                        (error, results) => {
-                            if (error) {
-                                return connection.rollback(() => {
-                                    req.status(500).json({ error });
-                                });
-                            }
-
-                            const insertId = results.insertId;
-                            const Phone_Number = Phone_Number.map(Phone_Number => [insertId, Phone_Number]);
-                            const E_Mail = E_Mail.map(E_Mail => [insertId, E_Mail]);
-                            const Business_Number = Business_Number.map(Business_Number => [insertId, Business_Number]);
-
-                            connection.query(
-                                "INSERT INTO Chemist_Contacts(Phone_Number, E_Mail, Business_Number)",
-                                [Phone_Number, E_Mail, Business_Number],
-                                (error, results) => {
-                                    if (error) {
-                                        return connection.rollback(() => {
-                                            req.statusCode(500).json({ error });
-                                        });
-                                    }
-
-                                    const insertId = results.insertId;
-                                    const id = id.map(id => [insertId, id]);
-                                    const Business_Number = Business_Number.map(Business_Number => [insertId, Business_Number]);
-                                    const Drug_Number = Drug_Number.map(Drug_Number => [insertId, Drug_Number]);
-                                    const Working_Hours = Working_Hours.map(Working_Hours => [insertId, Working_Hours]);
-
-                                    connection.query(
-                                        "INSERT INTO Business_Time(id, Business-Number, Drug_Number, Working-Hours)"
-                                        [id, Business - Number, Drug_Number, Working - Hours],
-                                        (error, results) => {
-                                            if (error) {
-                                                return connection.rollback(() => {
-                                                    req.statusCode(500).json({ error });
-                                                });
-                                            }
-
-                                            connection.commit(error => {
-                                                if (error) {
-                                                    return connection.rollback(() => {
-                                                        res.status(500).json({ error });
-                                                    });
-                                                }
-                                                connection.release();
-                                                res.json(insertId)
-                                            });
-
-                                        }
-                                    )
-                                }
-
-                            )
-                        }
-                    )
-
-                    
-                }
-            )
-            
-            
-        });
-    });
-
-});
+}); 
 
 app.put("/api/Chemist/id", (req, res) => {
     const { Chemist, Location, Business_Number } = req.body;
