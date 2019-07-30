e1e9dba1407eab9b39ab0d5c847930ffa1c7aa7f
TKEugene <eugenewem@gmail.com>
2019-07-30 13:23:44 +0300

    Search biox functuional in Drug page

--- client/src/DrugList.js
+++ client/src/DrugList.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Drug from "./Drug";
 import axios from "axios";
+import search from "./searchicon.png";
 
 
 class DrugList extends React.Component {
-
     constructor(props){
         super(props);
         this.state = {
             thedrug:[],
+            filteredDrugs: [],
+            searchString: "",
             loading: false,
             error: false,
         }
+        this.searchDrugs = this.searchDrugs.bind(this);
     }
 
     componentDidMount(){
@@ -23,34 +26,55 @@ class DrugList extends React.Component {
             loading: true, 
             error: false
         })
-        axios.get("/api/drug/")
-        .then(response =>(
-            this.setState({
-                thedrug: response.data, 
-                loading: false, 
-                error: false
+        axios.get("/api/Drug/")
+            .then(response =>(
+                this.setState({
+                     thedrug: response.data, 
+                     loading: false, 
+                     error: false
         
-            })
-        ))
+                 })
+             ))
 
-        .catch(error =>{
-            this.setState({
-                thedrug: [], 
-                loading: false,
-                error: true
+            .catch(error =>{
+                this.setState({
+                    thedrug: [], 
+                    loading: false,
+                    error: true
+               })
             })
-        })
 
     }
+
+    searchDrugs(e) {
+        const searchString = e.target.value;
+        const { thedrug } = this.state;
+
+        this.setState({
+            searchString,
+            filteredDrugs: thedrug.filter(d => {
+                return d.drug.toLowerCase().indexOf(searchString.toLowerCase()) > -1
+            }),
+        });
+    }
+
     render(){
-        const{thedrug} = this.state;
+        const {thedrug, searchString, filteredDrugs} = this.state;
+        const drugs = searchString === "" ? thedrug : filteredDrugs;
+
         return (
-        <div className = "thedrug">
-            {thedrug.map(king =>(
-                <Drug thedrug = {king}/>
-            ))}
-        </div>
-    )}
+            <div className = "searchbox">
+                <input id="input" type="text" placeholder="Search..." autoComplete="off" onChange={this.searchDrugs} />
+                <div className = "thedrug">
+                    {drugs.map(king =>(
+                        <Drug key = {king.Drug_Number} thedrug = {king} />
+
+                    ))}
+                </div>
+            </div>    
+        
+        )
+    }
 }
 
 
