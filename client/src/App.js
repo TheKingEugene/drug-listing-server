adc799fffa5e8720a3a7903d1aa6abee01b46860
TKEugene <eugenewem@gmail.com>
2019-07-31 16:53:47 +0300

    Added Buttons and a Messege to Admin page.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import DrugList from "./DrugList";
 import ChemistList from "./ChemistList";
-// import Admin from "./Admin"
+import Admin from "./Admin"
 import "./App.css";
 
 function App() {
@@ -19,6 +19,7 @@ function App() {
           <Home path = "/home" />
           <DrugList path = "/Drug" /> 
           <ChemistList path = "/Chemist" />
+          <Admin path = "/Admin"/>
         </Router>
       </main>
     </div>
